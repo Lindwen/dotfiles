3353348c844dfa2bb04733e48b84cbe46f983fd7
Lindwen <commit@lindwen.fr>
2025-08-15 15:07:25 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,33 @@
 #!/bin/bash
+set -e
 
 sudo apt update
-sudo apt install zsh git curl lsd btop tmux vim bat -y
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+sudo apt install -y zsh git curl lsd btop tmux vim bat
+
 chsh -s "$(command -v zsh)"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
-sed -i 's/^ZSH_THEME=".*"/ZSH_THEME="powerlevel10k\/powerlevel10k"/' ~/.zshrc
-cat <<'EOF' > "$ZSH_CUSTOM/aliases.zsh"
-alias ls='lsd -lah'
-alias cls='clear'
-EOF
-sed -i '/^# HIST_STAMPS="mm\/dd\/yyyy"/a HIST_STAMPS="%d-%m-%Y %H:%M:%S"' ~/.zshrc
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/fdellwing/zsh-bat.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-bat
-git clone https://github.com/MichaelAquilina/zsh-you-should-use.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/you-should-use
-sed -i 's/^plugins=(\(.*\))/plugins=(\1 docker zsh-autosuggestions zsh-syntax-highlighting zsh-bat you-should-use)/' ~/.zshrc
-source ~/.zshrc
+
+RUNZSH=no zsh -c 'sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended'
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git \
+    "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+sed -i 's|^ZSH_THEME=".*"|ZSH_THEME="powerlevel10k/powerlevel10k"|' "$HOME/.zshrc"
+
+mkdir -p "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
+curl -fsSL https://raw.githubusercontent.com/Lindwen/dotfiles/refs/heads/main/aliases.zsh -o "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/aliases.zsh"
+curl -fsSL https://raw.githubusercontent.com/Lindwen/dotfiles/refs/heads/main/.tmux.conf -o "$HOME/.tmux.conf"
+curl -fsSL https://raw.githubusercontent.com/Lindwen/dotfiles/refs/heads/main/.vimrc -o "$HOME/.vimrc"
+
+sed -i '/^# HIST_STAMPS="mm\/dd\/yyyy"/a HIST_STAMPS="%d-%m-%Y %H:%M:%S"' "$HOME/.zshrc"
+
+git clone https://github.com/zsh-users/zsh-autosuggestions \
+    "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
+    "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
+git clone https://github.com/fdellwing/zsh-bat.git \
+    "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-bat"
+git clone https://github.com/MichaelAquilina/zsh-you-should-use.git \
+    "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/you-should-use"
+
+sed -i 's/^plugins=(\(.*\))/plugins=(\1 docker zsh-autosuggestions zsh-syntax-highlighting zsh-bat you-should-use)/' "$HOME/.zshrc"
+
+exec zsh
