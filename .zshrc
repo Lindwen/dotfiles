d5d1ce9bf1ba5e84caf2ca5f99567bc166fe4455
Lindwen <34105327+Lindwen@users.noreply.github.com>
2025-11-12 10:39:33 +0100

    add fastfetch

--- .zshrc
+++ .zshrc
@@ -1,3 +1,8 @@
+# Launch fastfetch
+if [[ -o interactive ]]; then
+  fastfetch
+fi
+
 # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
 # Initialization code that may require console input (password prompts, [y/n]
 # confirmations, etc.) must go above this block; everything else may go below.
--- install.sh
+++ install.sh
@@ -39,8 +39,6 @@ cp /tmp/dotfiles/.tmux.conf "$HOME/.tmux.conf"
 cp /tmp/dotfiles/.vimrc "$HOME/.vimrc"
 cp /tmp/dotfiles/.zshrc "$HOME/.zshrc"
 cp /tmp/dotfiles/.p10k.zsh "$HOME/.p10k.zsh"
-sudo cp /tmp/dotfiles/99-fastfetch-motd "/etc/update-motd.d/99-fastfetch-motd"
-sudo chmod +x /etc/update-motd.d/99-fastfetch-motd
 
 rm -rf /tmp/dotfiles
 
